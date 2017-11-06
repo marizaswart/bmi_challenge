13f9b1b43159720e82f04b341c39ad3474760b12
marizaswart <mariza.swart@gmail.com>
2017-11-06 15:55:03 +0100

    Set up first steps for the bmi calculator, define messages and basic function

---
+++ src/person.js
@@ -0,0 +1,10 @@
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+}
+Person.prototype.calculate_bmi = function() {
+  //this.bmiValue = 26.01;
+  //this.bmiMessage = 'Overweight';
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+};

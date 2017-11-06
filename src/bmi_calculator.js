50cffb13ccf07c628c1bac4a3ddd297a13d9396e
marizaswart <mariza.swart@gmail.com>
2017-11-06 20:59:17 +0100

    Add files that are required to set up the BMI Challenge

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -5,7 +5,7 @@ BMICalculator.prototype.metric_bmi = function(obj) {
   var weight = obj.weight;
   var height = obj.height;
   if (weight > 0 && height > 0) {
-    var finalBMI = weight / (height / 100 * height / 100);
+    var finalBmi = weight / (height / 100 * height / 100);
     obj.bmiValue = parseFloat(finalBmi.toFixed(2));
     setBMIMessage(obj);
   }

fb80a24d9cf8f971a90bb2ad337e377bd3c63b71
marizaswart <mariza.swart@gmail.com>
2017-11-07 15:23:00 +0100

    Remove files that are not required, push file and jasmine to server

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -3,5 +3,16 @@ describe('BMI_UI - index.html', function() {
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('index.html');
     $.holdReady(false);
+    $('#weight').val('90');
+    $('#height').val('186');
+    $('#calculate').trigger('click');
+  });
+
+  it("displays BMI Value", function() {
+    expect($('#display_value').text()).toBe('Your BMI is 26.01');
+  });
+
+  it("display BMI Message", function() {
+    expect($('#display_message').text()).toBe('and you are Overweight');
   });
 });
